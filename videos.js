947dd1dbfb57fcd334f84357ca866a1e0b96ceaf
LewisB13 <lewisb13@protonmail.com>
2026-03-22 23:36:23 +0000

    Added video files externaly so i just need to code the js file

---
+++ videos.js
@@ -0,0 +1,17 @@
+const videos = [
+  {
+    title: "Intro Tutorial",
+    description: "A simple starter tutorial card.",
+    embedUrl: "https://www.youtube-nocookie.com/embed/dQw4w9WgXcQ?rel=0"
+  },
+  {
+    title: "Project Walkthrough",
+    description: "A second tutorial example in the grid.",
+    embedUrl: "https://www.youtube-nocookie.com/embed/M7lc1UVf-VE?rel=0"
+  },
+  {
+    title: "Learning Resource",
+    description: "Add more video cards here later as your site grows.",
+    embedUrl: "https://www.youtube-nocookie.com/embed/ysz5S6PUM-U?rel=0"
+  }
+];
